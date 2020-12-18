dd09db0dd6c6873a6d22811dfbc999ebd133939a
Fomagran <fomagran6@naver.com>
2020-12-18 14:13:09 +0900

    메모 뷰컨트롤러들 생성

---
+++ ViewController/MemoEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoEditViewController.swift
+//  RxSwift Memo
+//
+//  Created by Fomagran on 2020/12/18.
+//
+
+import UIKit
+
+class MemoEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController/MemoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MemoListViewController.swift
+//  RxSwift Memo
+//
+//  Created by Fomagran on 2020/12/18.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+
+
+}
