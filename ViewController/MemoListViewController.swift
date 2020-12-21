31ef0cfd4433da3937988c803bb6d7cd9dd1d67b
Fomagran <fomagran6@naver.com>
2020-12-21 13:11:53 +0900

    초기 뷰컨트롤러 설정

--- ViewController/MemoListViewController.swift
+++ ViewController/MemoListViewController.swift
@@ -6,19 +6,31 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController,ViewModelBindableType {
     
+    @IBOutlet weak var addButton: UIBarButtonItem!
     var viewModel:MemoListViewModel!
     
-
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
   
     func bindViewModel() {
-        
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        viewModel.memoList
+            .bind(to: table.rx.items(cellIdentifier: "ListCell")) {
+                row,memo,cell in cell.textLabel?.text = memo.content
+            }
+            .disposed(by: rx.disposeBag)
     }
 
 
