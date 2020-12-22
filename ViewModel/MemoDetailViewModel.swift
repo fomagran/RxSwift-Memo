e249fc4135dd28f9fd46a6f1b849c0145dbc87ac
Fomagran <fomagran6@naver.com>
2020-12-22 11:59:33 +0900

    메모디테일뷰모델 구현 및 뷰컨트롤러에 바인딩

--- ViewModel/MemoDetailViewModel.swift
+++ ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,31 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel:CommonViewModel {
+    
+    let memo:Memo
+    
+    private var formatter:DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "Ko_kr")
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+    
+    var contents:BehaviorSubject<[String]>
+    
+    init(memo:Memo,title:String,sceneCoordinator:SceneCoordinatorType,storage:MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [memo.content,formatter.string(from: memo.insertDate)])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+        
+    }
     
 }
