88d831b328566c51afb1a9d6fc4fe00d008fa019
Fomagran <fomagran6@naver.com>
2020-12-22 13:07:46 +0900

    메모 공유시 액티비티에 아무것도 안뜨는거 디버그

--- ViewController/MemoDetailViewController.swift
+++ ViewController/MemoDetailViewController.swift
@@ -51,7 +51,7 @@ class MemoDetailViewController: UIViewController,ViewModelBindableType {
         shareButton.rx.tap
             .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
             .subscribe(onNext:{[weak self] _ in
-                guard let memo = self?.viewModel.contents else {return}
+                guard let memo = self?.viewModel.memo.content else {return}
                 
                 let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
                 self?.present(vc, animated: true, completion: nil)
