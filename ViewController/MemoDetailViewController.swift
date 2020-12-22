087fd5b5e270e400c94c491935e3de32cb8a1a3b
Fomagran <fomagran6@naver.com>
2020-12-22 11:45:34 +0900

    MemoDetail storyboard 구성 및 연결

--- ViewController/MemoDetailViewController.swift
+++ ViewController/MemoDetailViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class MemoDetailViewController: UIViewController,ViewModelBindableType {
+    //MARK:Properties
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var table: UITableView!
     var viewModel:MemoDetailViewModel!
+    //MARK:LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
