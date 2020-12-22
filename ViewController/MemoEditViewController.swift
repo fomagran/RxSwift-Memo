ce938220afa10278abfbc89a1ba8675e7d57ee85
Fomagran <fomagran6@naver.com>
2020-12-22 11:29:01 +0900

    쓰기 뷰컨트롤러 기능 바인딩

--- ViewController/MemoEditViewController.swift
+++ ViewController/MemoEditViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
 
 class MemoEditViewController: UIViewController ,ViewModelBindableType{
+
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
     var viewModel:MemoEditViewModel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +23,34 @@ class MemoEditViewController: UIViewController ,ViewModelBindableType{
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
+            
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
     }
 
 
